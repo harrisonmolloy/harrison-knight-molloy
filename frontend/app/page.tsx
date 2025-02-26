76ca9b49a07cffa8bb5b17f5eadfca635d817d96
Harrison Knight Molloy <mail@harrisonmolloy.com>
2025-02-27 10:05:12 +1300

    chore: run prettier

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
   return (
     <>
       <main className="overflow-hidden pt-[140px] pl-10">
-        <h1 className="text-base font-normal leading-5">{configData.title}</h1>
-        <p className="text-base font-normal leading-5">{configData.tagline}</p>
+        <h1 className="text-base leading-5 font-normal">{configData.title}</h1>
+        <p className="text-base leading-5 font-normal">{configData.tagline}</p>
         <br />
         <a
           href="mailto:mail@harrisonmolloy.com"
-          className="text-base font-normal leading-5"
+          className="text-base leading-5 font-normal"
         >
           mail@harrisonmolloy.com
         </a>
