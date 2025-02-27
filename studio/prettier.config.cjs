3fb045ab164cf75accfbc2631fc5847008f76290
Harrison Knight Molloy <mail@harrisonmolloy.com>
2025-02-27 15:13:41 +1300

    chore: config prettier for studio

---
+++ studio/prettier.config.cjs
@@ -0,0 +1,7 @@
+const preset = require('@sanity/prettier-config')
+
+module.exports = {
+  ...preset,
+  plugins: [...preset.plugins, 'prettier-plugin-tailwindcss'],
+  experimentalTernaries: true,
+}
