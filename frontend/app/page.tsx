b9b0a04437b6594b3eb9c69571a347c9131ee00a
Harrison Knight Molloy <mail@harrisonmolloy.com>
2025-02-27 15:04:01 +1300

    style: main page basic styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const configData = await client.fetch(CONFIG_QUERY);
 
   return (
-    <main className="mt-40 pl-10">
+    <main className="mt-40 ml-8 max-w-md rounded-lg bg-gray-200 p-8">
       <h1>{configData.title}</h1>
       <p>{configData.tagline}</p>
       <br />
